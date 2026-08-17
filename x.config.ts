6daa59efe0bc2cfd38aed7029f90ab4088d0f614
Abdelkabir Ouadoukou <abdelkabir.ouadoukou@gmail.com>
2026-08-17 18:35:10 +0100

    init project

---
+++ x.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "@thexjs/core";
+
+export default defineConfig({
+  pagesDir: "src/pages",
+  layoutsDir: "src/layouts",
+  apiDir: "src/api",
+  contentDir: "content",
+  port: 3000,
+});
